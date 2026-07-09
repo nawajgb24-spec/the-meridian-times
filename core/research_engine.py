61f6ccafe88f60fce2e5cf6569a1526221b24754
nawajgb24-spec <nawajgb24@gmail.com>
2026-07-09 22:24:54 +0530

    Connect research parser

--- core/research_engine.py
+++ core/research_engine.py
@@ -1,16 +1,24 @@
 from core.gemini_engine import engine
+from core.parser import parser
 
 
 class ResearchEngine:
 
     def generate(self, topic: str):
 
-        return engine.generate(
+        response = engine.generate(
+
             "researcher",
+
             {
+
                 "TOPIC": topic
+
             }
+
         )
+
+        return parser.research(response)
 
 
 research = ResearchEngine()
