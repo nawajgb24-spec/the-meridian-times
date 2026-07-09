16dd268361f753cdadaf4c855d9242a87452acb2
nawajgb24-spec <nawajgb24@gmail.com>
2026-07-09 17:20:40 +0530

    Integrate news engine

--- static_blogger.py
+++ static_blogger.py
@@ -1,1 +1,29 @@
-print("The Meridian Times Engine")
+#!/usr/bin/env python3
+
+from core.logger import logger
+from core.config import config
+from core.news_fetcher import news_fetcher
+
+
+def main():
+
+    logger.info("===================================")
+    logger.info("The Meridian Times Engine Started")
+    logger.info("===================================")
+
+    categories = config.get("categories")
+
+    logger.info(f"Loaded {len(categories)} categories.")
+
+    for category in categories:
+
+        topics = news_fetcher.fetch(category)
+
+        logger.info(f"{category}: {len(topics)} topics")
+
+    logger.info("Engine Finished")
+
+
+if __name__ == "__main__":
+
+    main()
