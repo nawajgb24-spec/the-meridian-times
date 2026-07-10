1ae91c0ad32569fe46430b24b7e7b6099ae005a4
nawajgb24-spec <nawajgb24@gmail.com>
2026-07-10 20:51:26 +0530

    Update publisher.py

--- core/publisher.py
+++ core/publisher.py
@@ -2,11 +2,20 @@ from core.audit_logger import audit_logger
 from core.database import database
 from core.homepage_builder import homepage_builder
 from core.html_builder import html_builder
+from core.image_generator import image_generator
 
 
 class Publisher:
 
     def publish(self, article):
+
+        image_info = image_generator.generate(
+            article
+        )
+
+        article.featured_image = image_info[
+            "filename"
+        ]
 
         database.add(article)
 
